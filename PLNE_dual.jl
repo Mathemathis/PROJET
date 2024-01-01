e8e27352b60e570fee4911aa9d0daf282e75280a
Mathemathis <108059148+Mathemathis@users.noreply.github.com>
2024-01-01 22:24:45 +0100

    second commit with checks

--- PLNE_dual.jl
+++ PLNE_dual.jl
@@ -68,8 +68,8 @@ function plne_dual(n::Int64, s::Int64, t::Int64, S::Int64, d1::Int64, d2::Int64,
     if feasibleSolutionFound
         # Récupération des valeurs d’une variable 
         # println("Value x : ", JuMP.value.(x))
-        return JuMP.value.(a), JuMP.value.(eta), JuMP.value.(gamma)
         println("Valeur de l’objectif : ", JuMP.objective_value(m))
+        return JuMP.value.(x), JuMP.value.(a), JuMP.value.(eta), JuMP.value.(gamma)
     end
 end
 
@@ -96,6 +96,56 @@ function check_dual_poids(n::Int64, d2::Int64, p::Vector{Int64}, ph::Vector{Int6
     redirect_stdout(logfile)
 
     @objective(m, Max, sum(a[i]*(p[i]+delta_2[i]*ph[i]) for i in 1:n))
+
+    # Résolution d’un modèle
+    optimize!(m)
+
+    feasibleSolutionFound = primal_status(m) == MOI.FEASIBLE_POINT
+    isOptimal = termination_status(m) == MOI.OPTIMAL
+
+    println(primal_status(m))
+    println(termination_status(m))
+
+    if feasibleSolutionFound
+        println("Valeur de l’objectif : ", JuMP.objective_value(m))
+        return ("Valeur de l’objectif : ", JuMP.objective_value(m))
+    end
+end
+
+function check_dual_objective(n::Int64, d1::Int64, d::Dict{Any, Any}, D::Dict{Any, Any}, x, timelimit)
+
+    deltap=Dict()
+    deltam=Dict()
+    for i in 1:n
+        deltap[i]=[]
+        deltam[i]=[]
+    end
+    for (i,j) in keys(d)
+        push!(deltap[i],j)
+        push!(deltam[j],i)
+    end
+
+    m = Model(CPLEX.Optimizer)
+
+    @variable(m, delta_1[i in 1:n, j in deltap[i]]>=0)    
+
+    @constraint(m,  [i in 1:n, j in deltap[i]], delta_1[i,j]<=D[i,j])
+
+    @constraint(m,  sum(delta_1[i,j] for i in 1:n for j in deltap[i])<=d1)
+    
+    set_optimizer_attribute(m, "CPXPARAM_Preprocessing_Presolve", 0)
+    set_optimizer_attribute(m, "CPXPARAM_TimeLimit", timelimit)
+    set_optimizer_attribute(m, "CPX_PARAM_THREADS", 1)
+    set_optimizer_attribute(m, "CPXPARAM_MIP_Display", 4)
+
+    logfile_name = "check_dual_objective.txt"
+
+    # Obtenir le chemin absolu du fichier de journal dans le répertoire actuel
+    logfile_path = abspath(logfile_name)
+    logfile = open(logfile_path, "w")
+    redirect_stdout(logfile)
+
+    @objective(m, Max, sum(x[i,j]*d[i,j]*(1+delta_1[i,j]) for i in 1:n for j in deltap[i]))
 
     # Résolution d’un modèle
     optimize!(m)
