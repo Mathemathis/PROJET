4169a61850dfe4a936cd55abdd1f6d13fd4c5272
agathelhm <agathe.lhermite@live.fr>
2024-01-29 11:06:25 +0100

    si tu veux tester @Mathis

--- heuristic.jl
+++ heuristic.jl
@@ -34,6 +34,8 @@ function plneCompacte(n::Int64, s::Int64, t::Int64, S::Int64, p::Vector{Int64}, 
 
     @objective(m, Min, sum(x[i,j]*d[i,j] for i in 1:n for j in deltap[i]))
 
+    println("optimize_begin")
+
     # Résolution d’un modèle
     optimize!(m)
 
@@ -41,6 +43,8 @@ function plneCompacte(n::Int64, s::Int64, t::Int64, S::Int64, p::Vector{Int64}, 
 
     if feasibleSolutionFound
         return JuMP.value.(x), JuMP.value.(a)
+    else
+        @warn "Le PLNE constructif n'est pas realisable !!!! "
     end
 end
 
@@ -70,8 +74,11 @@ function constrSol(n, s, t, S, p, d, ph, d2, timelimit, name_instance, deltap, d
 
     @objective(m, Min, 0)
 
+    println("init_plne")
+
     # solution de départ
     initial_values=plneCompacte(n, s, t, S, p, d, name_instance, "No", 60, deltap, deltam)
+    println("plneCompacte_solved")
     JuMP.set_start_value.(x, initial_values[1])
     JuMP.set_start_value.(a, initial_values[2])
     
@@ -81,13 +88,17 @@ function constrSol(n, s, t, S, p, d, ph, d2, timelimit, name_instance, deltap, d
     set_optimizer_attribute(m, "CPXPARAM_MIP_Display", 4)
 
     # Résolution du modèle
+    println("optimize2_init")
     optimize!(m)
+    println("optimize2_finish")
 
     feasibleSolutionFound = primal_status(m) == MOI.FEASIBLE_POINT
 
     if feasibleSolutionFound
         # Récupération des valeurs d’une variable 
         return JuMP.value.(x), JuMP.value.(a)
+    else
+        @warn "Le PLNE constructif et dual n'est pas realisable !!!! "
     end
 end
 
@@ -158,6 +169,7 @@ function nvResPoids(i_ph_dec, i_to_i_ph_dec, sum_poids, i_lim, nv_noeud, old_noe
 end
 
 function VoisAmeliorant(chemin, i_ph_dec, i_to_i_ph_dec, sum_poids, i_lim, d2, ph, p, d1, d, D, longueur, deltap, deltam, S, sum_arcs)
+    """Trouve un voisin ameliorant (old_noeud, nv_noeud) ou retourne (-1, -1)"""
     i = 2
     while (i <= longueur-1) 
         sommets_admissibles = intersect(deltap[chemin[i-1]], deltam[chemin[i+1]]) # il existe un chemin
@@ -166,10 +178,12 @@ function VoisAmeliorant(chemin, i_ph_dec, i_to_i_ph_dec, sum_poids, i_lim, d2, p
         for nv_noeud in collect(sommets_admissibles)
             nv_poids = nvResPoids(i_ph_dec, i_to_i_ph_dec, sum_poids, i_lim, nv_noeud, chemin[i], d2, ph, p, longueur) # se fait en temps constant (youpi !)
             if nv_poids <= S
+                println("Solution admissible")
                 if nvDist(chemin, nv_noeud, chemin[i], d1, d, D) < sum_arcs # temps lineaire en le nombre d'aretes
-                    println("on a trouve une solution ameliorante")
+                    println("\nOn a trouve une solution ameliorante")
                     println("nv_noeud admissible ? ", isAdmissible(chemin, nv_noeud, chemin[i], d2, p, ph, deltap, S))
-                    println("nv distances ? ", nvDist(chemin, nv_noeud, chemin[i], d1, d, D))
+                    println("nv sum_dist = ", nvDist(chemin, nv_noeud, chemin[i], d1, d, D))
+                    println("nv sum_poids = ", nv_poids)
                     return(chemin[i], nv_noeud)
                 end
             end
@@ -180,60 +194,56 @@ function VoisAmeliorant(chemin, i_ph_dec, i_to_i_ph_dec, sum_poids, i_lim, d2, p
 end
 
 function deplacement(chemin, i_ph_dec, i_to_i_ph_dec, sum_poids, sum_arcs, longueur, i_lim, d1, d2, p, ph, d, D, old_noeud, nv_noeud)
-    
+    """on enleve old noeud du chemin et on met nv_noeud, retourne les informations"""
+    # i_lim, longueur restent égaux
+    i_old_noeud = i_to_i_ph_dec[old_noeud]
+    sum_poids = nvResPoids(i_ph_dec, i_to_i_ph_dec, sum_poids, i_lim, nv_noeud, old_noeud, d2, ph, p, longueur) # utiliser le temps constant
+
+    chemin = nvChemin(chemin, old_noeud, nv_noeud)
+
+    i_ph_dec =sort(chemin, lt = (x, y) -> ph[x] >= ph[y])
+    i_to_i_ph_dec=Dict() # passer du numero du sommet a sa position dans i_ph_dec
+    for i in collect(chemin)
+        i_to_i_ph_dec[i]= findfirst(x -> x == i, i_ph_dec)
+    end
+    sum_arcs =  Dist(chemin, d1, d, D)
+    return(chemin, i_ph_dec, i_to_i_ph_dec, sum_poids, sum_arcs)
+end
+
+function voisinages(name_instance)
+
+    # preparation solution
+    n, s, t, S, d1, d2, p, ph, d, D = read_file("./data/$name_instance")
+    deltap, deltam = initDelta(d, n)
+    timelimit = 30
+    x, a =constrSol(n, s, t, S, p, d, ph, d2, timelimit, name_instance, deltap, deltam)
+    chemin, i_ph_dec, i_to_i_ph_dec  = transformSol(a, n, s, t, ph, d, p, deltap, deltam)
+    longueur = length(chemin)
+    sum_poids, i_lim = getInfoSommets(chemin, p, ph, d2)
+    sum_arcs = Dist(chemin, d1, d, D)
+    println("Solution admissible")
+    println("sum_dist = ", sum_arcs)
+    println("sum_poids = ", sum_poids, ", S = ", S,  "\n")
+
+    # debut des voisinages
+    condition = true
+    while condition 
+        old_noeud, nv_noeud = VoisAmeliorant(chemin, i_ph_dec, i_to_i_ph_dec, sum_poids, i_lim, d2, ph, p, d1, d, D, longueur, deltap, deltam, S, sum_arcs)
+        println("old_noeud = ", old_noeud, " nv_noeud = ", nv_noeud, "\n")
+        if old_noeud == -1
+            condition = false
+        else
+            chemin, i_ph_dec, i_to_i_ph_dec, sum_poids, sum_arcs = deplacement(chemin, i_ph_dec, i_to_i_ph_dec, sum_poids, sum_arcs, longueur, i_lim, d1, d2, p, ph, d, D, old_noeud, nv_noeud)
+        end
+    end
 end
 
 function main()
-    name_instance="100_USA-road-d.BAY.gr"
+    #name_instance="100_USA-road-d.BAY.gr"
+    name_instance="1600_USA-road-d.COL.gr"
     n, s, t, S, d1, d2, p, ph, d, D = read_file("./data/$name_instance")
-
-    # init delta p delta m
-    deltap=Dict()
-    deltam=Dict()
-    for i in 1:n
-        deltap[i]=[]
-        deltam[i]=[]
-    end
-    for (i,j) in keys(d)
-        push!(deltap[i],j)
-        push!(deltam[j],i)
-    end
-
+    deltap, deltam = initDelta(d, n)
     timelimit = 30
     x, a =constrSol(n, s, t, S, p, d, ph, d2, timelimit, name_instance, deltap, deltam)
-    println("solution est construite")
-    #=
-    ph[14] = 7
-    p[15] = 18
-    ph[15] = 6
-    d2 = 5 =#
-
-    chemin, i_ph_dec, i_to_i_ph_dec  = transformSol(a, n, s, t, ph, d, p, deltap, deltam)
-    println("chemin = ", chemin)
-    println("i_ph_dec = ", i_ph_dec)
-    println("i_to_i_ph_dec = ", i_to_i_ph_dec)
-
-    longueur = length(chemin)
-
-    sum_poids, i_lim = getInfoSommets(chemin, p, ph, d2)
-    #=
-    nv_noeud = 15
-    old_noeud = 14
-
-    nv_poids = nvResPoids(i_ph_dec, i_to_i_ph_dec, sum_poids, i_lim, nv_noeud, old_noeud, d2, ph, p, longueur)
-    println("nv_poids = ", nv_poids)
-
-    nv_chemin = nvChemin(chemin, old_noeud, nv_noeud)
-    nv_poids_test, _ = getInfoSommets(nv_chemin, p, ph, d2)
-    println("nv_poids_test = ", nv_poids_test)
-    =#
-
-
-   
-    sum_arcs = Dist(chemin, d1, d, D)
-    println("sum_arcs  = ", sum_arcs)
-    println("chemin = ", chemin)
-
-   old_noeud, nv_noeud = VoisAmeliorant(chemin, i_ph_dec, i_to_i_ph_dec, sum_poids, i_lim, d2, ph, p, d1, d, D, longueur, deltap, deltam, S, sum_arcs)
-
+    #voisinages(name_instance)
 end
